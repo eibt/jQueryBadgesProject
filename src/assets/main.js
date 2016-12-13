2d0dda0a34fefc65cffff7bd94e026baf691b362
Mr. Ivan <ivan@ivandigital.com>
2016-12-13 16:13:55 -0600

    Complete tasks

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/ivandigital.json',
+        dataType: 'jsonp',
+        success: function(response) {
+            response.courses.completed.forEach(function(current) {
+                $('#badges').append('<div class="course"><h3>'
+                    + current.title + '</h3><img src="' + current.badge
+                    + '"><a target="_blank" class="btn-primary btn" href="'
+                    + current.url + '">See Course</a></div>');
+            });
+        }
+    });
 
 });
